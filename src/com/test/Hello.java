ec43681b7cc4182dbbb1ec6a0d440d8e37ae2374
sriharsha-at-git <harshu.1982@gmail.com>
2019-02-08 15:04:55 +0530

    Base code

---
+++ src/com/test/Hello.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String message = "Hi, How hard do you work !";
+		System.out.println("[MESSAGE ] :: "+message);
+
+	}
+
+}
